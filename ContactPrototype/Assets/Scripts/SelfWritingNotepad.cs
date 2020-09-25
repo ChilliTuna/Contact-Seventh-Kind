f8f618427196024b207fa81a17ad3da5f4a93477
Declan Rogers <declanrogers2420@gmail.com>
2020-09-25 14:27:35 +1000

    Text appending in the notepad
    + Changes to the script for the notepad from here it should be smooth sailing when we get object interactions flowing

--- ContactPrototype/Assets/Scripts/SelfWritingNotepad.cs
+++ ContactPrototype/Assets/Scripts/SelfWritingNotepad.cs
@@ -1,19 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Text
+using TMPro;
 
 public class SelfWritingNotepad : MonoBehaviour
 {
+    public TextMeshPro tm;
+    TextMeshProUGUI tmm;
+    string Text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tmm = gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Text += "W";
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Text += "A";
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Text += "S";
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Text += "D";
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Text += " ";
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Text += "\n";
+        }
+        tmm.text = Text;
     }
 }
