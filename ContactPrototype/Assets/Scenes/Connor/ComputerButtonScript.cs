916fc2d67c676de46d8c6444af74b11b192369bf
Connor <connormccaskie@gmail.com>
2020-10-08 12:57:25 +1100

    Created ability to move between pages, began working on login auth.

---
+++ ContactPrototype/Assets/Scenes/Connor/ComputerButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerButtonScript : MonoBehaviour
+{
+    public GameObject fromScreen;
+    public GameObject toScreen;
+    public Button thisButton;
+
+    private void OnEnable()
+    {
+        thisButton.onClick.AddListener(() => DoButtonFunctions());
+    }
+
+    private void DoButtonFunctions()
+    {
+        ChangeMenus();
+    }
+
+    private void OnDisable()
+    {
+        thisButton.onClick.RemoveAllListeners();
+    }
+
+    public void ChangeMenus()
+    {
+        fromScreen.SetActive(false);
+        toScreen.SetActive(true);
+    }
+
+}
--- ContactPrototype/Assets/Scenes/Connor/ComputerMainScript.cs
+++ ContactPrototype/Assets/Scenes/Connor/ComputerMainScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ComputerMainScript : MonoBehaviour
 {
-    public List<User> users;
+    public List<User> users = new List<User>();
 
     public string User1Name;
     public string User1Password;
@@ -15,16 +15,35 @@ public class ComputerMainScript : MonoBehaviour
     public string User4Name;
     public string User4Password;
 
+    private GameObject loginScreen1;
+    private GameObject loginScreen2;
+    private GameObject emailScreen1;
+    private GameObject emailScreen2;
+    private GameObject notesScreen1;
+    private GameObject notesScreen2;
+    private GameObject appsScreen2;
+
+    private User activeUser;
+
+    public enum EnumUsers
+    {
+        User1,
+        User2,
+        User3,
+        User4
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         CreateUsers();
+        GetChildMenus();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 
     private void CreateUsers()
@@ -33,6 +52,40 @@ public class ComputerMainScript : MonoBehaviour
         users.Add(new User(User2Name, User2Password));
         users.Add(new User(User3Name, User3Password));
         users.Add(new User(User4Name, User4Password));
+    }
+
+    private void GetChildMenus()
+    {
+        loginScreen1 = gameObject.transform.Find("Login Screen").gameObject;
+        loginScreen2 = gameObject.transform.Find("Login Screen 2").gameObject;
+        emailScreen1 = gameObject.transform.Find("Email Screen").gameObject;
+        emailScreen2 = gameObject.transform.Find("Email Screen 2").gameObject;
+        notesScreen1 = gameObject.transform.Find("Notes Screen").gameObject;
+        notesScreen2 = gameObject.transform.Find("Notes Screen 2").gameObject;
+        appsScreen2 = gameObject.transform.Find("Apps Screen").gameObject;
+    }
+
+    public void ChooseUser(EnumUsers p_user)
+    {
+        activeUser = ConvertEnumToUser(p_user);
+    }
+
+    private User ConvertEnumToUser(EnumUsers p_user)
+    {
+        switch (p_user)
+        {
+            case EnumUsers.User1:
+                return users[0];
+            case EnumUsers.User2:
+                return users[1];
+            case EnumUsers.User3:
+                return users[2];
+            case EnumUsers.User4:
+                return users[3];
+            default:
+                break;
+        }
+        return null;
     }
 }
 
