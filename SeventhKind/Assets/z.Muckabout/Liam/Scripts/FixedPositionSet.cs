5bc7d8a9c3aec355fcee8f6fdb5c738321e76398
Liam Smith <liamkevinsmithinbox@gmail.com>
2020-10-22 14:23:08 +1100

    - All older scripts are working - Updated both player grabbing scripts with a variable for the tag they use. - Added script to be able to force a player into a fixed position.

---
+++ SeventhKind/Assets/z.Muckabout/Liam/Scripts/FixedPositionSet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedPositionSet : MonoBehaviour
+{
+    // A class used for forcing the player into a position and rotation. 
+    // add this to an object that the player's "arm" will touch and click such as a box around the computer
+    // GrabFixedPosition is a script which interacts with this script 
+
+    // position and rotation is where you would like the player to be. 
+    public Vector3 desiredLocation;
+    public Vector3 deisredRotation;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
