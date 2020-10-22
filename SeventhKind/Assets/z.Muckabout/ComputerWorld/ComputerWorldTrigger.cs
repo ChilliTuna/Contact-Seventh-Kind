f2bedaa34df067f6fea53a4431f33756fc4b0a3d
Connor <connormccaskie@gmail.com>
2020-10-22 12:27:13 +1100

    Implemented dynamic world into computer system

---
+++ SeventhKind/Assets/z.Muckabout/ComputerWorld/ComputerWorldTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerWorldTrigger : MonoBehaviour
+{
+    private ComputerMainScript computer;
+    public GameObject[] worldStates;
+    private int currentWorldState = 0;
+
+    private void Start()
+    {
+        computer = GetComponent<ComputerMainScript>();
+    }
+
+    public void ProgressWorldState()
+    {
+        if (!computer.activeUser.hasProgressed)
+        {
+            worldStates[currentWorldState].SetActive(true);
+            currentWorldState++;
+            computer.activeUser.hasProgressed = true;
+        }
+    }
+}
