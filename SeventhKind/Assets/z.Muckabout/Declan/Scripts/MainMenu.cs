3157934478801af9453185721e2ad6255fc7dd19
Baz Lever <bazlever6@gmail.com>
2020-11-04 10:55:42 +1100

    Updated Main

---
+++ SeventhKind/Assets/z.Muckabout/Declan/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void MenuButton(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+    
+    
+  
+
+
+    public void MenuExitButton(int SceneNumber)
+    {
+        Application.Quit();
+    }
+}
+
---
+++ SeventhKind/Assets/z.Muckabout/Declan/Scripts/MainMenuTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("MENU");
+        SceneManager.LoadScene(0);
+    }
+}
