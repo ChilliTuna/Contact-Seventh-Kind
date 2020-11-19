6c1ed2faa711878348572f7ad36bb19619a6eb07
Baz Lever <bazlever6@gmail.com>
2020-11-19 15:57:18 +1100

    Added game

---
+++ SeventhKind/Assets/_Main/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    public void MenuButton(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+    
+    
+  
+
+
+    public void MenuExitButton()
+    {
+        Application.Quit();
+    }
+}
+
