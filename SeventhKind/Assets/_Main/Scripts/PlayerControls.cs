a749799b763b0cca231ead96edf7480819cd80e4
Connor <connormccaskie@gmail.com>
2020-11-26 16:21:05 +1100

    Added player forced to move

--- SeventhKind/Assets/_Main/Scripts/PlayerControls.cs
+++ SeventhKind/Assets/_Main/Scripts/PlayerControls.cs
@@ -15,9 +15,15 @@ public class PlayerControls : MonoBehaviour
     public bool isMovementDisabled = false;
 
     public Rigidbody OurRigid;
+    public CameraToMouse camerTarget;
 
     float hAxis;
     float vAxis;
+
+    public bool isLockedIntoFinalScene;
+    public UnityEngine.Vector3 finalSceneLocation;
+
+    public float finalSceneMovement;
 
     void Start()
     {
@@ -29,6 +35,13 @@ public class PlayerControls : MonoBehaviour
     {
         hAxis = 0;
         vAxis = 0;
+
+        if (isLockedIntoFinalScene)
+        {
+            FinalSceneMovement();
+        }
+        else
+        {
 
         if (!isMovementDisabled)
         {
@@ -66,7 +79,76 @@ public class PlayerControls : MonoBehaviour
             OurRigid.useGravity = true;
         }
 
+        }
     }
+
+
+    void FinalSceneMovement()
+    {
+        float xValue = 0;
+        float yValue = 0;
+        float zValue = 0;
+        OurRigid.useGravity = false;
+
+        camerTarget.TurnOffUse();
+
+        //x
+        if ((this.transform.position.x + finalSceneMovement * Time.deltaTime > finalSceneLocation.x)
+            && (this.transform.position.x - finalSceneMovement * Time.deltaTime < finalSceneLocation.x))
+        {
+            this.transform.position = new UnityEngine.Vector3(finalSceneLocation.x, this.transform.position.y, this.transform.position.z);
+        }
+        else if (this.transform.position.x < finalSceneLocation.x)
+        {
+            xValue = finalSceneMovement;
+        }
+        else
+        {
+            xValue = -finalSceneMovement;
+        }
+
+        //y
+        if ((this.transform.position.y + finalSceneMovement * Time.deltaTime > finalSceneLocation.y) 
+            && (this.transform.position.y - finalSceneMovement * Time.deltaTime < finalSceneLocation.y))
+        {
+            this.transform.position = new UnityEngine.Vector3(this.transform.position.x, finalSceneLocation.y, this.transform.position.z);
+        }
+        else if (this.transform.position.y < finalSceneLocation.y)
+        {
+            yValue = finalSceneMovement;
+        }
+        else
+        {
+            yValue = -finalSceneMovement;
+        }
+
+        //z
+        if ((this.transform.position.z + finalSceneMovement * Time.deltaTime > finalSceneLocation.z)
+            && (this.transform.position.z - finalSceneMovement * Time.deltaTime < finalSceneLocation.z))
+        {
+            this.transform.position = new UnityEngine.Vector3(this.transform.position.x, this.transform.position.y, finalSceneLocation.z);
+        }
+        else if (this.transform.position.z < finalSceneLocation.z)
+        {
+            zValue = finalSceneMovement;
+        }
+        else
+        {
+            zValue = -finalSceneMovement;
+        }
+
+        //actually move
+
+        UnityEngine.Vector3 amountToMove = new UnityEngine.Vector3(xValue,yValue,zValue).normalized;
+
+        this.transform.position = new UnityEngine.Vector3(this.transform.position.x + (xValue * finalSceneMovement *Time.deltaTime), this.transform.position.y + (yValue * finalSceneMovement * Time.deltaTime), this.transform.position.z + (zValue * finalSceneMovement * Time.deltaTime));
+
+
+      // OurRigid.position += amountToMove.x * transform.forward * finalSceneMovement * Time.deltaTime;
+      //  OurRigid.position += amountToMove.y * transform.right * finalSceneMovement * Time.deltaTime;
+        //OurRigid.position += amountToMove.y * transform.up * finalSceneMovement * Time.deltaTime;
+    }
+
 
     void FixedUpdate()
     {
