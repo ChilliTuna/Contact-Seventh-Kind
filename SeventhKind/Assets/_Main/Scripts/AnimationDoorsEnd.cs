1310a2fb886c44d0ebff7374e79da9e4965f94c1
Declan Rogers <declanrogers2420@gmail.com>
2020-11-26 15:23:31 +1100

    Door Opens Music added

---
+++ SeventhKind/Assets/_Main/Scripts/AnimationDoorsEnd.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDoorsEnd : MonoBehaviour
+{
+
+
+    public Animator doorAnim;
+    public AudioClip Normal;
+    public AudioClip MedBay;
+    AudioSource audio;
+    bool hasRun = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Normal;
+        audio.loop = true;
+        audio.Play();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasRun)
+        {
+            hasRun = true;
+            audio.loop = false;
+            audio.volume = 0.2f;
+            doorAnim.SetBool("IsOpening", true);
+            audio.clip = MedBay;
+            audio.Play();
+
+        }
+    }
+}
+
